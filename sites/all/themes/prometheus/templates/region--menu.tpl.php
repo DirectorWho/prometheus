191aa6b3911b0fe8a54df9e6e21453af0f079af9
John Hannah <john@friendlymachine.net>
2014-05-12 13:29:07 -0400

    test

--- sites/all/themes/prometheus/templates/region--menu.tpl.php
+++ sites/all/themes/prometheus/templates/region--menu.tpl.php
@@ -1,5 +1,5 @@
 <div<?php print $attributes; ?>>
-  <div<?php print $content_attributes; ?>>
+  <div<?php print $content_attributes; ?> id="what">
     <?php if ($main_menu || $secondary_menu): ?>
         <?php print theme('links__system_main_menu', array('prefix' => '<nav><a href="#menu" class="menu-link">Menu</a>', 'links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'main-menu')), 'heading' => array('text' => t('Main menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
         <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'secondary-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
