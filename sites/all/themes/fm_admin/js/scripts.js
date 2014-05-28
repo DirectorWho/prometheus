1412421b47d2aa1df0a520eaecd24a8d866a880c
John Hannah <john@friendlymachine.net>
2014-05-28 18:04:15 -0400

    add jquery to show/hide fields on campus content type, take 9

--- sites/all/themes/fm_admin/js/scripts.js
+++ sites/all/themes/fm_admin/js/scripts.js
@@ -10,7 +10,6 @@ Drupal.behaviors.my_custom_behavior = {
 
     $("#edit-field-campus-type-und").change(function() {
       var currentValue = $( "#edit-field-campus-type-und option:selected" ).text();
-      alert(currentValue);
       if (currentValue == "CSU" || currentValue == "UC") {
         $(".conditional").css("display", "block");
       }
